eaf58e9b5a13f37d210e0c1530eb0b257ee2619d
Vinh Pham <vinh.pham@spiritlabs.co>
2020-02-20 14:55:47 +0700

    Fix copyright

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -6,8 +6,8 @@ const Copyright = () => {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      <Link color="inherit" href="https://jorvinbook.firebaseapp.com/">
+        Jorvinbook Social Network
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
