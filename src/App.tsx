07dd2ddd2dd184524f514da353efe7d12829fac6
Vinh Pham <vinh.pham@spiritlabs.co>
2020-02-20 14:14:43 +0700

    Add sign in UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { ThemeOptions } from "@material-ui/core/styles/createMuiTheme";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
+import utils from "./utils";
+import SignIn from "./pages/SignIn";
+
+const { styles, theme: themeObject } = utils;
+const { Container } = styles;
+
+const theme = createMuiTheme(themeObject as ThemeOptions);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Container>
+          <Switch>
+            <Route exact path="/sign-in" component={SignIn} />
+          </Switch>
+        </Container>
+      </Router>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+import * as styles from "./styles";
+import theme from "./theme";
+
+export default { styles, theme } 
