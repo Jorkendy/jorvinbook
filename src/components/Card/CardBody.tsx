5b77318ed1934fff48a4843bdf0f1ec0fb6eb507
Vinh Pham <vinh.pham@spiritlabs.co>
2020-02-24 18:51:37 +0700

    Complete UI profile

--- src/components/Card/CardBody.tsx
+++ src/components/Card/CardBody.tsx
@@ -6,18 +6,28 @@ const useStyles = makeStyles({
   cardBody: {
     padding: "0.9375rem 20px",
     flex: "1 1 auto",
-    position: "relative"
+    position: "relative",
   },
   cardBodyProfile: {
     marginTop: "15px"
+  },
+  cardBodyCenter: {
+    padding: "0.9375rem 20px",
+    flex: "1 1 auto",
+    position: "relative",
+    textAlign: "center"
   }
 });
 
-const CardBody: FC = ({ children, ...rest }) => {
+const CardBody: FC<any> = ({ children, type, ...rest }) => {
   const classes = useStyles();
+  const className =
+    type === "center"
+      ? clsx(classes.cardBodyCenter, classes.cardBodyProfile)
+      : clsx(classes.cardBody, classes.cardBodyProfile);
 
   return (
-    <div className={clsx(classes.cardBody, classes.cardBodyProfile)} {...rest}>
+    <div className={className} {...rest}>
       {children}
     </div>
   );
--- src/pages/UserProfile/UserProfile.tsx
+++ src/pages/UserProfile/UserProfile.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import {
+  KeyboardDatePicker,
+  MuiPickersUtilsProvider
+} from "@material-ui/pickers";
+import MomentUtils from "@date-io/moment";
+import MenuItem from "@material-ui/core/MenuItem";
+import SaveIcon from "@material-ui/icons/Save";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 
 import GridContainer from "../../components/Grid/GridContainer";
 import GridItem from "../../components/Grid/GridItem";
@@ -8,10 +19,9 @@ import CardHeader from "../../components/Card/CardHeader";
 import CardBody from "../../components/Card/CardBody";
 import CardFooter from "../../components/Card/CardFooter";
 import CardAvatar from "../../components/Card/CardAvatar";
-import Button from "@material-ui/core/Button";
-import ava from "../../assets/images/user.jpg";
+import defaultAvatar from "../../assets/images/user.jpg";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   cardCategoryWhite: {
     color: "rgba(255,255,255,.62)",
     margin: "0",
@@ -27,14 +37,38 @@ const useStyles = makeStyles({
     fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
     marginBottom: "3px",
     textDecoration: "none"
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  input: {
+    display: "none"
+  },
+  cardBodyCategory: {
+    fontFamily: "Roboto,Helvetica,Arial,sans-serif",
+    fontWeight: 300,
+    lineHeight: "1.5em",
+    fontSize: "1em",
+    textTransform: "uppercase"
+  },
+  cardBodyTitle: {
+    fontFamily: "Roboto,Helvetica,Arial,sans-serif",
+    fontWeight: 300,
+    lineHeight: "1.5em",
+    fontSize: "1em"
+  },
+  description: {
+    lineHeight: "1.5em",
+    fontSize: "1.5em"
   }
-});
+}));
 
 const UserProfile = () => {
   const classes = useStyles();
 
   return (
-    <div>
+    <Wrapper>
       <GridContainer>
         <GridItem xs={12} sm={12} md={8}>
           <Card>
@@ -43,69 +77,156 @@ const UserProfile = () => {
               <p className={classes.cardCategoryWhite}>Complete your profile</p>
             </CardHeader>
             <CardBody>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={5}>
-                  a
-                </GridItem>
-                <GridItem xs={12} sm={12} md={3}>
-                  a
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  a
-                </GridItem>
-              </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={6}>
-                  a
-                </GridItem>
-                <GridItem xs={12} sm={12} md={6}>
-                  a
-                </GridItem>
-              </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={4}>
-                  a
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  a
-                </GridItem>
-                <GridItem xs={12} sm={12} md={4}>
-                  a
-                </GridItem>
-              </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={12}>
-                  avatar
-                </GridItem>
-              </GridContainer>
+              <form className={classes.form} noValidate>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <TextField
+                      variant="outlined"
+                      margin="normal"
+                      required
+                      fullWidth
+                      id="firstName"
+                      label="First name"
+                      name="firstName"
+                      autoFocus
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <TextField
+                      variant="outlined"
+                      margin="normal"
+                      required
+                      fullWidth
+                      id="lastName"
+                      label="Last name"
+                      name="lastName"
+                      autoFocus
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <TextField
+                      variant="outlined"
+                      margin="normal"
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email"
+                      name="email"
+                      autoFocus
+                    />
+                  </GridItem>
+                </GridContainer>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <TextField
+                      variant="outlined"
+                      margin="normal"
+                      fullWidth
+                      id="company"
+                      label="Company"
+                      name="company"
+                      autoFocus
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <MuiPickersUtilsProvider utils={MomentUtils}>
+                      <KeyboardDatePicker
+                        label="Birthday"
+                        margin="normal"
+                        value={null}
+                        fullWidth
+                        allowKeyboardControl
+                        animateYearScrolling
+                        onChange={() => {}}
+                        disableFuture
+                        inputVariant="outlined"
+                        clearable
+                        placeholder="10/10/2018"
+                        format="MM/dd/yyyy"
+                      />
+                    </MuiPickersUtilsProvider>
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <TextField
+                      id="standard-select-currency"
+                      select
+                      label="Gender"
+                      fullWidth
+                      margin="normal"
+                      variant="outlined"
+                    >
+                      <MenuItem key={1} value={1}>
+                        Male
+                      </MenuItem>
+                      <MenuItem key={2} value={2}>
+                        Female
+                      </MenuItem>
+                      <MenuItem key={3} value={3}>
+                        Others
+                      </MenuItem>
+                    </TextField>
+                  </GridItem>
+                </GridContainer>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <input
+                      accept="image/*"
+                      className={classes.input}
+                      id="avatar"
+                      multiple
+                      type="file"
+                    />
+                    <label htmlFor="avatar">
+                      <Button
+                        variant="contained"
+                        color="default"
+                        startIcon={<CloudUploadIcon />}
+                        component="span"
+                      >
+                        Upload your avatar
+                      </Button>
+                    </label>
+                  </GridItem>
+                </GridContainer>
+              </form>
             </CardBody>
             <CardFooter>
-              <Button color="primary">Update Profile</Button>
+              <Button
+                color="primary"
+                variant="contained"
+                startIcon={<SaveIcon />}
+              >
+                Update Profile
+              </Button>
             </CardFooter>
           </Card>
         </GridItem>
         <GridItem xs={12} sm={12} md={4}>
           <Card>
             <CardAvatar>
-              <a href="#pablo" onClick={e => e.preventDefault()}>
-                <img src={ava} alt="..." />
-              </a>
+              <img src={defaultAvatar} alt="..." />
             </CardAvatar>
-            <CardBody>
-              <h6>CEO / CO-FOUNDER</h6>
-              <h4>Alec Thompson</h4>
-              <p>
+            <CardBody type="center">
+              <h6 className={classes.cardBodyCategory}>CEO / CO-FOUNDER</h6>
+              <h4 className={classes.cardBodyTitle}>Alec Thompson</h4>
+              <p className={classes.description}>
                 Don{"'"}t be scared of the truth because we need to restart the
                 human foundation in truth And I love you like Kanye loves Kanye
                 I love Rick Owens’ bed design but the back is...
               </p>
-              <Button>Follow</Button>
+              <Button color="primary" variant="contained">
+                Follow
+              </Button>
             </CardBody>
           </Card>
         </GridItem>
       </GridContainer>
-    </div>
+    </Wrapper>
   );
 };
 
 export default UserProfile;
+
+const Wrapper = styled.div`
+  margin-top: 50px;
+`;
